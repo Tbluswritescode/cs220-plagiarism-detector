34caef434b4ab699f17d50e7c210a766bb4d29b0
T <tblus@outlook.com>
2022-05-11 20:04:39 -0500

    started read file, filled out some delegation functions

--- src/plagdetect/PlagiarismDetector.java
+++ src/plagdetect/PlagiarismDetector.java
@@ -1,38 +1,41 @@
 package plagdetect;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 
 public class PlagiarismDetector implements IPlagiarismDetector {
-	
+	private int N = 3;
+	private Map<String, Map<String, Integer>> results;
+	private Map<String, Set<String>> allNgrams;
+	private Set<String> recentFile;
+
 	public PlagiarismDetector(int n) {
-		// TODO implement this method
+		results = new HashMap<String, Map<String, Integer>>();
+		allNgrams = new HashMap<String, Set<String>>();
 	}
 	
 	@Override
 	public int getN() {
-		// TODO Auto-generated method stub
-		return 0;
+		return N;
 	}
 
 	@Override
 	public Collection<String> getFilenames() {
-		// TODO Auto-generated method stub
-		return null;
+		return allNgrams.keySet();
 	}
 
 	@Override
 	public Collection<String> getNgramsInFile(String filename) {
-		// TODO Auto-generated method stub
-		return null;
+		readFile(filename);
+		return allNgrams.get(filename);
 	}
 
 	@Override
 	public int getNumNgramsInFile(String filename) {
-		// TODO Auto-generated method stub
-		return 0;
+		return allNgrams.get(filename).size();
 	}
 
 	@Override
@@ -41,10 +44,41 @@ public class PlagiarismDetector implements IPlagiarismDetector {
 		return null;
 	}
 
+	public void readFile(String filename){
+		File file = new File(filename);
+		readFile(file);
+		allNgrams.add(filename, recentFile);
+		recentFile = new Set<String>();
+	}
+
 	@Override
 	public void readFile(File file) throws IOException {
 		// TODO Auto-generated method stub
 		// most of your work can happen in this method
+		try {
+			Set<String> fileNgrams = new HashSet<String>();
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+			  String line = scan.nextLine();
+			  for (int i = 0; i < line.length() - N; i++){
+				recentFile.add(line.substring(i, i+N));
+			  }
+			
+			for (Set<String> e : allNgrams.values()){
+				for (String s : e){
+					if (recentFile.contains(s)){
+						count += 1;
+					}
+				}
+
+			}
+		}
+		}
+		catch (FileNotFoundException e) {
+
+		}
+
+
 		
 	}
 
